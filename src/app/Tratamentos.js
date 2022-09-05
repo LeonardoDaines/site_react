5c931c0bf7f0e6de2019cc614b589f27d8b12807
leona <leonardodaines123@gmail.com>
2022-09-05 14:00:20 -0300

    segundo commit

--- src/app/Tratamentos.js
+++ src/app/Tratamentos.js
@@ -15,6 +15,10 @@ function Tratamentos(props){
                                 consectetur adip isicing elit, <br/>
                                 sed do eiusmod tempor incididunt ut<br/>
                                 labore et dolore magna aliqua.</p>
+                                <p>Lorem ipsum dolor sit amet,<br/>
+                                consectetur adip isicing elit, <br/>
+                                sed do eiusmod tempor incididunt ut<br/>
+                                labore et dolore magna aliqua.</p>
                              </div>
                         </button>
                     </div>  
